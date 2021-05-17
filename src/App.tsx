c8607242eea4a7ab66cfbbc3c90238f315746914
Ratovoson81 <Ratovoson81@gmail.com>
2021-05-17 17:05:31 +0300

    setup tailwind.css in progess

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
 import "./App.css";
 
 function App() {
@@ -18,6 +18,20 @@ function App() {
         >
           Learn React
         </a>
+        <p className="text-5xl">test</p>
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-semibold">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. It’s easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-cyan-600">Sarah Dayan</div>
+            <div className="text-gray-500">Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./api";
+import "./css/tailwind.css";
+import "tailwindcss/tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
