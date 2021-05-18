bfe911baae74c76a97cdf2dc9fd8638427f0c32d
Ratovoson81 <Ratovoson81@gmail.com>
2021-05-18 10:51:45 +0300

    init tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./api";
+import "./css/index.css";
+import "./css/App.css";
 import "./css/tailwind.css";
-import "tailwindcss/tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
