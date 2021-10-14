7ec311aba7e306099118c5f33fd05c8cc45c8738
ratovoson81 <ratovoson81@gmail.com>
2021-10-14 14:46:26 +0300

    affiche nav bar if connected

--- src/router/NavBar.tsx
+++ src/router/NavBar.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/Auth";
+import { authType } from "../CostumType";
 
 const item = classNames(
   "w-20",
@@ -12,28 +14,46 @@ const item = classNames(
 const active = classNames(item, "bg-gray-100", "rounded-lg");
 
 export default function NavBar() {
+  let auth = useAuth() as authType;
+
+  function Login() {
+    return (
+      <>
+        <li className={item}>
+          <NavLink exact activeClassName={active} to="/">
+            Login
+          </NavLink>
+        </li>
+        <li className={item}>
+          <NavLink activeClassName={active} to="/register">
+            Register
+          </NavLink>
+        </li>
+      </>
+    );
+  }
+
+  function Home() {
+    return (
+      <>
+        <li className={item}>
+          <NavLink activeClassName={active} to="/accueil">
+            Acceuil
+          </NavLink>
+        </li>
+        <li className={item}>
+          <NavLink activeClassName={active} to="/home">
+            Home
+          </NavLink>
+        </li>
+      </>
+    );
+  }
+
   return (
     <ul className="flex justify-end">
-      <li className={item}>
-        <NavLink exact activeClassName={active} to="/">
-          Login
-        </NavLink>
-      </li>
-      <li className={item}>
-        <NavLink activeClassName={active} to="/register">
-          Register
-        </NavLink>
-      </li>
-      <li className={item}>
-        <NavLink activeClassName={active} to="/accueil">
-          Acceuil
-        </NavLink>
-      </li>
-      <li className={item}>
-        <NavLink activeClassName={active} to="/home">
-          Home
-        </NavLink>
-      </li>
+      {!auth.user && <Login />}
+      {auth.user && <Home />}
     </ul>
   );
 }
