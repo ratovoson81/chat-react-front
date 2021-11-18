bcf0ebc0099540a007dff817f6defa4ce2cd3ebd
ratovoson81 <ratovoson81@gmail.com>
2021-11-18 04:25:14 +0300

    react flip move finish

--- src/components/chat/ListConversation.tsx
+++ src/components/chat/ListConversation.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from "../../Hooks";
 import { useListUserAndGroupe } from "../../services/ListUserAndGroupe";
 import TimeAgo from "timeago-react";
 import FlipMove from "react-flip-move";
+import { forwardRef } from "react";
 
 export default function ListConversation() {
   const groupes = useAppSelector((state) => state.groupe.groupes);
@@ -14,6 +15,40 @@ export default function ListConversation() {
     (state) => state.groupe.idselectedGroupe
   );
   const { selectConversation } = useListUserAndGroupe();
+
+  const List = forwardRef(({ item, user }: any, ref) => (
+    <div
+      ref={ref as any}
+      onClick={() => selectConversation(item)}
+      className={`${
+        item.id === idselectedGroupe && "bg-gray-100"
+      } grid grid-cols-5 grid-rows-1 h-16 rounded-xl cursor-pointer items-center max-w-full`}
+    >
+      <span className="relative inline-block">
+        <img
+          className="rounded-full col-span-1 m-auto"
+          width={55}
+          alt=""
+          src={IMAGE_URL + user?.imageUrl}
+        />
+        {user?.isOnline && (
+          <span className="absolute bottom-0.5 right-3 text-xs h-3 w-3 leading-none ring-2 ring-gray-50 transform bg-green-400 rounded-full"></span>
+        )}
+      </span>
+      <div className="flex flex-col col-span-3 pl-4 justify-center">
+        <div className="font-medium">{user?.name}</div>
+        <div className="text-gray-500 text-xs">
+          {item.messages[0]?.author.id === me.id && "Vous: "}
+          {item.messages[0] ? item.messages[0]?.content : "vide"}
+        </div>
+      </div>
+      <div className="flex flex-col col-span-1 justify-center items-end pr-4">
+        <div className="text-xs">
+          <TimeAgo datetime={item.messages[0]?.date} locale="pt_BR" />
+        </div>
+      </div>
+    </div>
+  ));
 
   return (
     <>
@@ -30,83 +65,12 @@ export default function ListConversation() {
               new Date(b.messages[0]?.date).getTime() -
               new Date(a.messages[0]?.date).getTime()
           )
-          .filter((item: Groupe) => item.id !== me.id)
           .map((item: Groupe, i: number) => {
             const id = item.users.find((i) => i.user?.id !== me.id)?.user?.id;
             const user = users.find((u) => id === u.id);
-            return (
-              <div
-                key={i}
-                onClick={() => selectConversation(item)}
-                className={`${
-                  item.id === idselectedGroupe && "bg-gray-100"
-                } grid grid-cols-5 grid-rows-1 h-16 rounded-xl cursor-pointer items-center max-w-full`}
-              >
-                <span className="relative inline-block">
-                  <img
-                    className="rounded-full col-span-1 m-auto"
-                    width={55}
-                    alt=""
-                    src={IMAGE_URL + user?.imageUrl}
-                  />
-                  {user?.isOnline && (
-                    <span className="absolute bottom-0.5 right-3 text-xs h-3 w-3 leading-none ring-2 ring-gray-50 transform bg-green-400 rounded-full"></span>
-                  )}
-                </span>
-                <div className="flex flex-col col-span-3 pl-4 justify-center">
-                  <div className="font-medium">{user?.name}</div>
-                  <div className="text-gray-500 text-xs">
-                    {item.messages[0]?.author.id === me.id && "Vous: "}
-                    {item.messages[0] ? item.messages[0]?.content : "vide"}
-                  </div>
-                </div>
-                <div className="flex flex-col col-span-1 justify-center items-end pr-4">
-                  <div className="text-xs">
-                    <TimeAgo datetime={item.messages[0]?.date} locale="pt_BR" />
-                  </div>
-                </div>
-              </div>
-            );
+            return <List key={item.id} item={item} user={user} />;
           })}
       </FlipMove>
     </>
   );
 }
-
-//version without grid
-/*          <div
-            key={i}
-            onClick={() => selectConversation(item)}
-            className={`${
-              item.id === idselectedGroupe && "bg-gray-100"
-            } flex h-16 rounded-xl cursor-pointer items-center`}
-          >
-            <div className="relative inline-block w-1/4">
-              <img
-                className="rounded-full"
-                width={55}
-                alt=""
-                src={
-                  IMAGE_URL +
-                  item.users.find((i) => i.user?.id !== me.id)?.user?.imageUrl
-                }
-              />
-              {item.users.find((i) => i.user?.id !== me.id)?.user?.isOnline && (
-                <span className="absolute bottom-0.5 right-0.5 text-xs h-3 w-3 leading-none ring-2 ring-gray-50 transform bg-green-400 rounded-full"></span>
-              )}
-            </div>
-            <div className="ml-4 justify-center w-full">
-              <div className="font-medium">
-                {item.users.find((i) => i.user?.id !== me.id)?.user?.name}
-              </div>
-              <div className="text-gray-500 text-xs">
-                {item.messages[0]?.author.id === me.id && "Vous: "}
-                {item.messages[0] ? item.messages[0]?.content : "vide"}
-              </div>
-            </div>
-            <div className="w-1/4 flex justify-center ">
-              <div className="text-xs">
-                <TimeAgo datetime={item.messages[0]?.date} locale="pt_BR" />
-              </div>
-            </div>
-          </div>*/
